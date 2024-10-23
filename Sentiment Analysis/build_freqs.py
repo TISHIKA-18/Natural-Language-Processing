c54e9ff2b0c4b267e63708b3113173c68d29f8e3
Tishika Babbar <67360217+TISHIKA-18@users.noreply.github.com>
2024-10-23 21:19:06 +0530

    build_freqs.py

---
+++ Sentiment Analysis/build_freqs.py
@@ -0,0 +1,18 @@
+def build_freqs(tweets, ys):
+    # Convert np array to list since zip needs an iterable.
+    # The squeeze is necessary or the list ends up with one element.
+    # Also note that this is just a NOP if ys is already a list.
+    yslist = np.squeeze(ys).tolist()
+
+    # Start with an empty dictionary and populate it by looping over all tweets
+    # and over all processed words in each tweet.
+    freqs = {}
+    for y, tweet in zip(yslist, tweets):
+        for word in process_tweet(tweet):
+            pair = (word, y)
+            if pair in freqs:
+                freqs[pair] += 1
+            else:
+                freqs[pair] = 1
+
+    return freqs
